ff5be0cc1abbebe560f9c60f75cd6960adc14730
Ola <olawunmi263@yahoo.com>
2020-05-19 04:06:16 +0100

    First Java Lecture

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+import com.company.Main;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    }
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       int myNumber = 88;
+        System.out.println(myNumber);
+        myNumber = 94;
+        System.out.println(myNumber);
+        String yoruba = "Emi ke";
+        System.out.println("yoruba");
+    }
+}
