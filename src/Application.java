76a7c5092edf915f0de4b264de8bb76876951759
Ola <olawunmi263@yahoo.com>
2020-05-21 06:31:38 +0100

    Some for loop and if included

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,11 @@
 import com.company.Main;
-
-public class Application {
+//For loop
+public class Application extends Main{
 
     public static void main(String[] args) {
-
+for (int pe =2;pe<10;pe++){
+    System.out.println("Hi bro");
+}
     }
 
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,31 @@
 package com.company;
-
+//while loop
 public class Main {
 
     public static void main(String[] args) {
-        int votersAge = 11;
-        System.out.println("Prepare to looop!!!");
-        while (votersAge < 16){
-            System.out.println("You cannot vote, you are " + votersAge + "!");
-            votersAge = votersAge +1;
+//        int votersAge = 16;
+//        System.out.println("Prepare to looop!!!");
+//        while (votersAge ==16){
+//            System.out.println("Congratulations, you can vote");
+//            votersAge = votersAge +1;
+//
+//        }
+//        System.out.println("Phew! That was fun");
 
+        // For Loop
+//        for (int i =2;i<10;i++){
+//            System.out.println("Hi bro " + i);
+//        }
+        //   }
+
+        int i = 1;
+        while (i < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            i = i + 1;
         }
-        System.out.println("Phew! That was fun");
+        if (i == 3) {
+            System.out.print("Do");
+        }
     }
 }
