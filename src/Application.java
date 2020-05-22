237735986f2c6fa41cb26637db991fa24645baa7
Ola <olawunmi263@yahoo.com>
2020-05-22 01:44:08 +0100

    Arrays attempt

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,36 @@
 import com.company.Main;
 //For loop
-public class Application extends Main{
+public class Application{
 
     public static void main(String[] args) {
-for (int pe =2;pe<10;pe++){
-    System.out.println("Hi bro");
-}
-    }
+//for (int pe =2;pe<10;pe++){
+//    System.out.println("Hi bro");}
+
+        }
+        //Make a list of random numbers
+        String[] describeOla1 = {"strong", "hardworking", "resilient", "intelligent","beautiful","kind", "driven"};
+        String[] describeOla2 = {"amiable", "energetic", "generous", "honest", "calm", "optimistic", "passionate",
+                "resourceful", "versatile"};
+        String[] describeOla3  = {"thoughtful", "independent", "determined","dynamic"};
+
+        //To find out how many words are in each list and save this number as listLength
+        int listLength1 = describeOla1.length;
+        int listLength2 = describeOla2.length;
+        int listLength3 = describeOla3.length;
+
+        //generate 3 random numbers
+        int rand1 = (int)(Math.random()*listLength1);
+// the words in the string are now being seen as numbers
+        //take a random number from that list
+        int rand2 = (int)(Math.random()*listLength2);
+        int rand3 = (int)(Math.random()*listLength3);
+
+// Now build a phrase
+
+        String phrase = describeOla1[rand1] + " " + describeOla2[rand2] + " " + describeOla3[rand3];
 
     }
+
+
+
+
---
+++ src/B.java
@@ -0,0 +1,34 @@
+import com.company.Main;
+
+public class B {
+    public static void main(String[] args) {
+
+    }
+
+    //Make a list of random numbers
+    String[] describeOla1 = {"strong", "hardworking", "resilient", "intelligent", "beautiful", "kind", "driven"};
+    String[] describeOla2 = {"amiable", "energetic", "generous", "honest", "calm", "optimistic", "passionate",
+            "resourceful", "versatile"};
+    String[] describeOla3 = {"thoughtful", "independent", "determined", "dynamic"};
+
+    //To find out how many words are in each list and save this number as listLength
+    int listLength1 = describeOla1.length;
+    int listLength2 = describeOla2.length;
+    int listLength3 = describeOla3.length;
+
+    //generate 3 random numbers
+    int rand1 = (int) (Math.random() * listLength1);
+    // the words in the string are now being seen as numbers
+    //take a random number from that list
+    int rand2 = (int) (Math.random() * listLength2);
+    int rand3 = (int) (Math.random() * listLength3);
+
+// Now build a phrase
+
+    String phrase = describeOla1[rand1] + " " + describeOla2[rand2] + " " + describeOla3[rand3];
+
+
+
+
+}
+
